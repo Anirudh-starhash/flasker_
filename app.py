fcbca9cac4b005d71f6460c9025304fc19a66363
Anirudh Pabbaraju <anirudhpabbaraju1103@gmail.com>
2024-05-12 23:55:30 +0530

    added

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField,SubmitField
+from wtforms.validators import DataRequired
 
+
+# create a flask instance
 app=Flask(__name__)
+app.config["SECRET_KEY"]="That does'nt concern me"
+
+# create a flask form
+class nameform(FlaskForm):
+    name=StringField("Whats your name",validators=[DataRequired()])
+    submit=SubmitField("Submit")
+
 
 @app.route("/")
 
@@ -8,7 +20,9 @@ app=Flask(__name__)
 #     return '<h1>Hello</h1>'
 
 def index():
-    return render_template("index.html")
+    stuff="This is <strong>Bold</strong> Text"
+    favorite_cars=['Audi','Mercedes','Ferrari','Lamborgini',41]
+    return render_template("index.html",stuff=stuff,favorite_cars=favorite_cars)
 
 @app.route("/user/<name>")
 
@@ -16,9 +30,7 @@ def index():
 #     return '<h1>Hello {} !!!</h1>'.format(name)
 
 def user(name):
-    stuff="This is <strong>Bold</strong> Text"
-    favorite_cars=['Audi','Mercedes','Ferrari','Lamborgini',41]
-    return render_template("user.html",name=name,stuff=stuff,favorite_cars=favorite_cars)
+    return render_template("user.html",name=name)
 
 # creating a custom error pages
 
@@ -33,6 +45,19 @@ def invalid(e):
 
 def internal_server(e):
     return render_template("500.html"),500
+
+@app.route("/name",methods=['GET','POST'])
+
+def name():
+    name=None
+    form=nameform()
+    
+    if form.validate_on_submit():
+        name=form.name.data
+        form.name.data=''
+        flash('Form Submitted Successfully')
+        
+    return render_template('name.html',form=form,name=name)
 
 
 
